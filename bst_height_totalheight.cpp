6c6312e577ae170ecc7ebf0bc791d2f654176e2d
Raj270892 <gupta.u.raj@gmail.com>
2018-02-18 14:35:44 -0500

    actual_answer

--- bst_height_totalheight.cpp
+++ bst_height_totalheight.cpp
@@ -4,12 +4,96 @@ using namespace std;
 
 vector<string> split_string(string);
 
+struct bstNode
+{
+	int data;
+	int count;
+	bstNode *left;
+	bstNode *right;
+};
+bstNode *createbstNode(int data)
+{
+	bstNode* root=new bstNode();
+	root->data=data;
+	root->left=root->right=NULL;
+	return root;
+}
+bstNode * insertbstNode(int data,bstNode *root)
+{
+	if(root==NULL)
+	{
+		root=createbstNode(data);
+	}
+	else if (data == root->data)
+	{
+		(root->count)++;
+	}	
+
+	else if(data > root->data)
+	{
+		root->right= insertbstNode(data,root->right);
+	}
+	else if(data <=root->data)
+	{
+		root->left= insertbstNode(data,root->left);
+	}
+	return root;
+}
+
+int depthsearch(bstNode *node)
+{
+	if (node==NULL) 
+		return -1;
+	else
+	{
+		/* compute the depth of each subtree */
+		int lDepth = depthsearch(node->left);
+		int rDepth = depthsearch(node->right);
+
+		/* use the larger one */
+		if (lDepth > rDepth) 
+			return(lDepth+1);
+		else
+			return(rDepth+1);
+	}
+
+}
+
+int fulldepthsearch(bstNode *node)
+{
+	if (node==NULL)
+		return 0;
+	else
+	{
+		/* compute the depth of each subtree */
+		int lDepth = fulldepthsearch(node->left);
+		int rDepth = fulldepthsearch(node->right);
+
+		/* use the larger one */
+	
+			return(rDepth+lDepth+depthsearch(node));
+	}
+}
 
 /*
  * Complete the function below.
  */
 vector<int> heightAndTotalHeight(vector<int> arr) {
     // Write your code here.
+    vector <int> result(2);
+    bstNode *root=NULL;
+    for(int i=0;i<arr.size();i++)
+    {
+    	root=insertbstNode(arr[i],root);
+    }
+    
+    int depth,l_node_depth,r_node_depth;
+    
+   result[0]=depthsearch(root);
+   
+   result[1]=fulldepthsearch(root);
+   
+    return result;
 
 
 }
@@ -36,6 +120,7 @@ int main()
     }
 
     vector<int> result = heightAndTotalHeight(arr);
+
 
     for (int result_itr = 0; result_itr < result.size(); result_itr++) {
         fout << result[result_itr];
